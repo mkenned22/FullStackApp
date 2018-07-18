402ac2b477e72edd75cf2df632bc5a4271beacd7
Mike Kennedy <mkenned22@gmail.com>
2018-07-17 20:44:35 -0400

    login page 5

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,17 +1,28 @@
 import React from "react";
-//import { Col, Row, Container } from "../../components/Grid";
-//import Jumbotron from "../../components/Jumbotron";
+import { Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
 
-class Login extends React.Component {
+class NoMatch extends React.Component {
 
   render() {
 
     return (
       <Container fluid>
-        <h1>Hello World</h1>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>Hello World!</h1>
+              <h1>
+                <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                  🙄
+                </span>
+              </h1>
+            </Jumbotron>
+          </Col>
+        </Row>
       </Container>
     )
   }
 }
 
-export default Login;
+export default NoMatch;
