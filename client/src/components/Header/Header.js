ab811305f7d490712b10534ae3e9d056122eae74
Mike Kennedy <mkenned22@gmail.com>
2018-07-17 21:09:29 -0400

    img src

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import 
 
 const Header = () =>
     <div class="page-header">
         <div class="col-md-4">
-            <img src="../../../public/SafePassage.png"/>
+            <img src="/SafePassage.png"/>
         </div>
         <div class="col-md-8">
             <h1>SafePassage <small>Start Your Next Jounrey</small></h1>
