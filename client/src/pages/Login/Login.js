254b801f3854f43643e4378e85d8ee5d338e8dee
Mike Kennedy <mkenned22@gmail.com>
2018-07-17 20:24:48 -0400

    login page

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import { Col, Row, Container } from "../../components/Grid";
+//import Jumbotron from "../../components/Jumbotron";
+
+class NoMatch extends React.Component {
+
+  render() {
+
+    return (
+      <Container fluid>
+        <h1>Hello World</h1>
+      </Container>
+    )
+  }
+}
+
+export default Login;
