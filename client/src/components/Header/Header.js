a679f2908e1ef0f8ce9b13862842e6b20d5077db
Mike Kennedy <mkenned22@gmail.com>
2018-07-17 20:58:19 -0400

    Header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () =>
+    <div class="page-header">
+        <div class="col-md-4">
+            <img src="%PUBLIC_URL%/SafePassage.png"/>
+        </div>
+        <div class="col-md-8">
+            <h1>SafePassage <small>Start Your Next Jounrey</small></h1>
+        </div>
+    </div>
+
+export default Header;
