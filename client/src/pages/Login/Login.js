9fe183e023c7f43587159aa915b36bcf38d0d8f7
Mike Kennedy <mkenned22@gmail.com>
2018-07-17 21:02:07 -0400

    Header

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row, Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
+import Header from "../../components/Header";
 
 class NoMatch extends React.Component {
 
@@ -10,14 +10,7 @@ class NoMatch extends React.Component {
       <Container fluid>
         <Row>
           <Col size="md-12">
-            <Jumbotron>
-              <h1>Hello World!</h1>
-              <h1>
-                <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                  🙄
-                </span>
-              </h1>
-            </Jumbotron>
+            <Header/>
           </Col>
         </Row>
       </Container>
